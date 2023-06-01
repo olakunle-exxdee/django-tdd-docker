5a71093468c6439e156ae471931f2e9f8475c465
Olakunle Ogunsolu <saheedogunsolu2016@gmail.com>
2023-06-01 16:40:40 +0200

    added pytest

--- app/movies/admin.py
+++ app/movies/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+
+from .models import Movie, CustomUser
+
+
+@admin.register(CustomUser)
+class UserAdmin(DefaultUserAdmin):
+    pass
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    fields = (
+        "title",
+        "genre",
+        "year",
+        "created_date",
+        "updated_date",
+    )
+    list_display = (
+        "title",
+        "genre",
+        "year",
+        "created_date",
+        "updated_date",
+    )
+    readonly_fields = (
+        "created_date",
+        "updated_date",
+    )
---
+++ app/tests/movies/test_serializers.py
@@ -0,0 +1,22 @@
+from movies.serializers import MovieSerializer
+
+
+def test_valid_movie_serializer():
+    valid_serializer_data = {"title": "lorem", "genre": "action", "year": "2012"}
+    serializer = MovieSerializer(data=valid_serializer_data)
+    assert serializer.is_valid()
+    assert serializer.validated_data == valid_serializer_data
+    assert serializer.data == valid_serializer_data
+    assert serializer.errors == {}
+
+
+def test_invalid_movie_serializer():
+    invalid_serializer_data = {
+        "title": "lorem",
+        "genre": "action",
+    }
+    serializer = MovieSerializer(data=invalid_serializer_data)
+    assert not serializer.is_valid()
+    assert serializer.validated_data == {}
+    assert serializer.data == invalid_serializer_data
+    assert serializer.errors == {"year": ["This field is required."]}
